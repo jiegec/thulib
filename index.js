fa8decfb02e4ee52a46266780bcfac50718bcf18
Jiajie Chen <jiegec@qq.com>
2018-09-15 10:50:21 +0800

    Support learn2018 and improve learn

--- index.js
+++ index.js
@@ -2,8 +2,12 @@ const AuthUtil = require('./lib/auth')
 const CicLearnHelperUtil = require('./lib/cic_learnhelper')
 const CurriculumUtil = require('./lib/curriculum')
 const LearnHelperUtil = require('./lib/learnhelper')
+const Learn2018HelperUtil = require('./lib/learn2018helper')
 
-module.exports = {AuthUtil: AuthUtil,
-                  CicLearnHelperUtil: CicLearnHelperUtil,
-                  CurriculumUtil: CurriculumUtil,
-                  LearnHelperUtil: LearnHelperUtil}
+module.exports = {
+    AuthUtil: AuthUtil,
+    CicLearnHelperUtil: CicLearnHelperUtil,
+    CurriculumUtil: CurriculumUtil,
+    LearnHelperUtil: LearnHelperUtil,
+    Learn2018HelperUtil: Learn2018HelperUtil
+}

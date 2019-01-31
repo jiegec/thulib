d848efa8f6a2bd5016c726d51efcec9269950c75
Jiajie Chen <jiegec@qq.com>
2019-01-31 20:56:06 +0800

    Use eslint

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
-const AuthUtil = require('./lib/auth')
-const CicLearnHelperUtil = require('./lib/cic_learnhelper')
-const CurriculumUtil = require('./lib/curriculum')
-const LearnHelperUtil = require('./lib/learnhelper')
-const Learn2018HelperUtil = require('./lib/learn2018helper')
+const AuthUtil = require('./lib/auth');
+const CicLearnHelperUtil = require('./lib/cic_learnhelper');
+const CurriculumUtil = require('./lib/curriculum');
+const LearnHelperUtil = require('./lib/learnhelper');
+const Learn2018HelperUtil = require('./lib/learn2018helper');
 
 module.exports = {
-    AuthUtil: AuthUtil,
-    CicLearnHelperUtil: CicLearnHelperUtil,
-    CurriculumUtil: CurriculumUtil,
-    LearnHelperUtil: LearnHelperUtil,
-    Learn2018HelperUtil: Learn2018HelperUtil
-}
+  AuthUtil,
+  CicLearnHelperUtil,
+  CurriculumUtil,
+  LearnHelperUtil,
+  Learn2018HelperUtil,
+};
